0a3ce5eb5dde7f448062d54bbc90845bb6cf9d56
André Rodrigues <79713707+Andre-Rodrigues-Dev-Web@users.noreply.github.com>
2021-03-18 03:09:41 -0300

    Third upload done, I added git ignore and uploaded the main file.

--- Blockchain/Program.cs
+++ Blockchain/Program.cs
@@ -2,11 +2,29 @@
 
 namespace Blockchain
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DependencyManager.Fill(new TransactionPool(), new BlockMiner(), new EmbedServer("5449"));
+            DependencyManager.BlockMiner.Start();
+            DependencyManager.EmbedServer.Start(); 
+            Console.ReadKey();
+            DependencyManager.BlockMiner.Stop();
+            DependencyManager.EmbedServer.Stop();
+        }
+    }
+    public static class DependencyManager
+    {
+        public static TransactionPool TransactionPool { get; private set; }
+        public static BlockMiner BlockMiner { get; private set; }
+        public static EmbedServer EmbedServer { get; private set; }
+
+        public static void Fill(TransactionPool transactionPool, BlockMiner blockMiner, EmbedServer embedServer)
+        {
+            TransactionPool = transactionPool;
+            BlockMiner = blockMiner;
+            EmbedServer = embedServer;
         }
     }
 }
